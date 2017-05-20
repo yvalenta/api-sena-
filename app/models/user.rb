71a0348e1f7c7002530109a4d2f6b2a62d04501e
Yonatan Valencia <yvalenta@gmail.com>
2017-05-19 20:59:40 -0500

    Adding validations, enums and secure_password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+
+  enum status: [:active, :inactive]
+  enum role: [:customer, :support, :admin]
+
+  has_secure_password
+
+  validates :username, presence: true, length: {within: 2..20}, uniqueness: true
+  validates :name, presence: true, length: {within: 5..20}
+  validates :email, presence: true, length: {within: 5..20}, uniqueness: true
+
 end
